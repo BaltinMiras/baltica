ba87b6e07586f25c451177857f376ba974172754
BaltinMiras <m_baltin@kbtu.kz>
2026-09-13 23:42:48 +0500

    Update casting.py

--- practice1/python-basics/casting.py
+++ practice1/python-basics/casting.py
@@ -1,1 +1,10 @@
-
+x = int(170)   # x will be 170
+y = int(2.81) # y will be 2
+z = int("3333") # z will be 3333
+x = float(14)     # x will be 14.0
+y = float(2223.8)   # y will be 2223.8
+z = float("34")   # z will be 34.0
+w = float("4.2") # w will be 4.2
+x = str("s1") # x will be 's1'
+y = str(2)    # y will be '2'
+z = str(3.0)  # z will be '3.0'
