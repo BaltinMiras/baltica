94ec98900b8be0fc2fadec65733b4c6dd705fbe3
BaltinMiras <m_baltin@kbtu.kz>
2026-09-14 14:33:03 +0500

    Update boolean_operators.py

--- practice2/boolean/boolean_operators.py
+++ practice2/boolean/boolean_operators.py
@@ -1,1 +1,5 @@
-
+print(bool("Hello")) #output is True
+print(bool(15)) #output is True
+print(bool(0)) #output is False
+print(bool("")) #output is False
+print(bool(["apple", "cherry", "banana"]) #output is True
