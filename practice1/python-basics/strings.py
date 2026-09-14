7263895cdad628104c55f1f3f0e8627f18c1c476
BaltinMiras <m_baltin@kbtu.kz>
2026-09-14 12:22:50 +0500

    Update strings.py

--- practice1/python-basics/strings.py
+++ practice1/python-basics/strings.py
@@ -1,3 +1,6 @@
 print("less go")
-print("if you need this>>'")
+print("if you need this quote>>'")
 print('miras"')
+txt = "It is so cheap!"
+if "expensive" not in txt:
+  print("No, 'expensive' is NOT present.")
