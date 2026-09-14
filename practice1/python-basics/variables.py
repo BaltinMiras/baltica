de5ae807cbad3588bc9be0ae26df25394fafb7cd
BaltinMiras <m_baltin@kbtu.kz>
2026-09-14 12:20:21 +0500

    Update variables.py

--- practice1/python-basics/variables.py
+++ practice1/python-basics/variables.py
@@ -1,3 +1,7 @@
 x = 67       # x is of type int
-x = "Miras" # x is now of type str
+y = "Miras" # x is now of type str
+n1 = str(3)    # x will be '3'
+n2 = int(3)    # y will be 3
+n3 = float(3)  # z will be 3.0
 print(x)
+
