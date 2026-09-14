f0130082dba40e0189a5bf694754dc4f9701b64b
BaltinMiras <m_baltin@kbtu.kz>
2026-09-14 11:51:45 +0500

    Update data_types.py

--- practice1/python-basics/data_types.py
+++ practice1/python-basics/data_types.py
@@ -1,2 +1,12 @@
-x = 555
-print(type(x))
+x_int = 555
+x_str = "Hello world"
+x_float = 45.777
+x_complex=5j
+x_list=["banana"]
+print(type(x_int))
+print(type(x_str))
+print(type(x_float))
+print(type(x_complex))
+print(type(x_list))
+
+
