64986282e2d0971c57b94ffc6e5a52f378320d28
BaltinMiras <m_baltin@kbtu.kz>
2026-09-20 22:18:16 +0500

    Add Person class with greet method

---
+++ Practice-03/classes/class_methods.py
@@ -0,0 +1,13 @@
+# Class method
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    def greet(self):
+        print(f"Hello, my name is {self.name}")
+
+
+person = Person("Miras")
+
+person.greet()
