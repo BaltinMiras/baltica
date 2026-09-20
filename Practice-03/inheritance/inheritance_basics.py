911c3349a715274051716c39ae5a6133bd498bf3
BaltinMiras <m_baltin@kbtu.kz>
2026-09-20 22:19:19 +0500

    Update inheritance_basics.py

---
+++ Practice-03/inheritance/inheritance_basics.py
@@ -0,0 +1,16 @@
+# Basic inheritance
+
+class Animal:
+    def eat(self):
+        print("Animal is eating")
+
+
+class Dog(Animal):
+    def bark(self):
+        print("Dog is barking")
+
+
+dog = Dog()
+
+dog.eat()
+dog.bark()
